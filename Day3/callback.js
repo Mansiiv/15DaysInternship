5a6b8ed7f2052b9b8f452c0e94f6bffc2ad38cda
Mansi Vekaria <mansivekaria09@gmail.com>
2021-06-03 10:24:30 +0530

    callback function

---
+++ Day3/callback.js
@@ -0,0 +1,13 @@
+//function
+function greet(name,callback){
+    console.log("Hello" + ' ' + name);
+    callback();
+}
+
+//callback function
+
+function callMe(){
+    console.log("I am callback function");
+}
+
+greet('Mansi',callMe);

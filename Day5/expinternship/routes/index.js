cdd6f10f5745a0c4d13ca543780293bdf82b2850
Mansi Vekaria <mansivekaria09@gmail.com>
2021-06-04 17:17:27 +0530

    photography page

--- Day5/expinternship/routes/index.js
+++ Day5/expinternship/routes/index.js
@@ -2,9 +2,6 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
 
 router.get('/about', function(req, res, next) {
   res.render('about');
@@ -12,6 +9,18 @@ router.get('/about', function(req, res, next) {
 
 router.get('/contact', function(req, res, next) {
   res.render('contact');
+});
+
+router.get('/', function(req, res, next) {
+  res.render('master');
+});
+
+router.get('/master', function(req, res, next) {
+  res.render('master');
+});
+
+router.get('/gallery', function(req, res, next) {
+  res.render('gallery');
 });
 
 module.exports = router;
